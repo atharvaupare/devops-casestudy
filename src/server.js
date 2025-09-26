7796e395a84f5b2feb0a4b57b2a0494d6c9a5313
atharvaupare <atharvaupare5@gmail.com>
2025-09-26 20:48:16 +0530

    deploy

--- src/server.js
+++ src/server.js
@@ -1,7 +1,10 @@
 require('dotenv').config();
 const app = require('./app');
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+// const PORT = process.env.PORT || 3000;
+// app.listen(PORT, () => {
+//   console.log(`Server is running on port ${PORT}`);
+// });
+
+
+module.exports = app;
